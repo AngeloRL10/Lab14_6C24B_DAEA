e68f63406fa83049cb825bb45682e9dcc0d74f0c
AngeloR10 <angelorl102004@gmail.com>
2024-06-29 20:58:12 -0500

    Agregar archivos de proyecto.

---
+++ Lab13_6C24B_DAEA/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Lab13_6C24B_DAEA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab13_6C24B_DAEA.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:sqlserver-almeyda.database.windows.net,1433;Initial Catalog=bdalmeyda;Persist Security Info=False;User ID=almeydadmin;Password=Admin123#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enrollment>()
+                .HasKey(e => e.IdEnrollment);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.StudentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.CourseId);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Grade)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.GradeId);
+
+            modelBuilder.Entity<Course>()
+                .HasKey(c => c.IdCourse);  // Configurar IdCourse como clave primaria
+        }
+    }
+}
